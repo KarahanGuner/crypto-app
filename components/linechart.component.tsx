1d144eea4ff92f2d8d117cc783847f7e4474b200
Kara6000 <aqwkarahann@gmail.com>
2021-08-23 18:25:48 +0300

    created the chart and options for it

--- components/linechart.component.tsx
+++ components/linechart.component.tsx
@@ -1,52 +1,53 @@
 import React from 'react'
 import {Line} from 'react-chartjs-2';
+import { ChartOptions } from 'chart.js'
+import 'chartjs-adapter-moment';
 
-
-const options = {
+const options : ChartOptions = {
     scales: {
       xAxis: {
-        // The axis for this scale is determined from the first letter of the id as `'x'`
-        // It is recommended to specify `position` and / or `axis` explicitly.
         type: 'time',
+        grid : {
+          display : false
       }
-    }
+      },
+      yAxis: {
+        ticks: {
+          callback: (label) => `$${label}`
+        },
+      },
+    },
+    plugins: {
+      tooltip: {
+        callbacks: {
+            label: function (context: {[key: string]: any}) {
+              if(context.raw.y > 1) {
+                return `$${context.raw.y.toFixed(2)}`;
+              } else {
+                return `$${Number(context.raw.y).toFixed(10)}`;
+              }
+            }
+        },
+        mode: 'index',
+        intersect: false
+      },
+      legend: {
+        display: false //This will do the task
+      }
+    },
+    
   }
 
 const LineChart: React.FC<any> = ({chartData}) => {
-    
-
     const data= {
         datasets: [
           {
             label: 'Price',
-            data: [{
-                x: new Date(2016, 5 ,1),
-                y: Math.random() * 100
-              },
-              {
-                x: new Date(2016, 6 ,1),
-                y: Math.random() * 100
-              },
-              {
-                x: new Date(2016, 7 ,1),
-                y: Math.random() * 100
-              },
-              {
-                x: new Date(2016, 8 ,1),
-                y: Math.random() * 100
-              },
-              {
-                x: new Date(2016, 9 ,1),
-                y: Math.random() * 100
-              },
-              {
-                x: new Date(2016, 10 ,1),
-                y: Math.random() * 100
-              },
-            ],
-            backgroundColor: "rgba(174, 305, 194, 0.5)",
-            borderColor: "rgba(174, 305, 194, 0.4)",
+            data: chartData,
+            backgroundColor: "#6fb2ff70",
+            borderColor: "#58a6ff",
             pointRadius: 0,
+            fill: true
           },
         ],
       } 
