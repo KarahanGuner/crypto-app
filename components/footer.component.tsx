07e0c6e6615a507c5d02ec10efc5c5aec8919b8a
Kara6000 <aqwkarahann@gmail.com>
2021-08-28 21:45:33 +0300

    made footer responsive, put the logo to the header on smaller widths

--- components/footer.component.tsx
+++ components/footer.component.tsx
@@ -1,28 +1,52 @@
-import React from 'react'
-import styles from '../styles/Footer.module.css';
-
+import React from "react";
+import styles from "../styles/Footer.module.css";
 
 const Footer: React.FC = () => {
-    return (
-        <footer className={styles.footer}>
-            <div>
-                <span>
-                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/KarahanGuner">   
-                    My GitHub 
-                    </a>
-                </span>
-                &nbsp;
-                <span>
-                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/karahan-g%C3%BCner-9b4b221a0/">
-                    My LinkedIn
-                    </a>
-                </span>
-                &nbsp;
-                <span>Email: kkarahanguner@gmail.com</span>
-              </div>
-            <div>Data provided by&nbsp;<a target="_blank" href="https://www.coingecko.com/" rel="noopener noreferrer">CoinGecko</a></div>
-        </footer>
-    )
-}
+  return (
+    <footer className={styles.footer}>
+      <div className={styles["footer__links-container"]}>
+        <div>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/KarahanGuner"
+          >
+            My GitHub
+          </a>
+        </div>
+        <div>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.karahanguner.com/"
+          >
+            Personal Website
+          </a>
+        </div>
+
+        <div>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/karahan-g%C3%BCner-9b4b221a0/"
+          >
+            My LinkedIn
+          </a>
+        </div>
+        <div>Email: kkarahanguner@gmail.com</div>
+      </div>
+      <div className={styles["footer__coingecko-container"]}>
+        Data provided by&nbsp;
+        <a
+          target="_blank"
+          href="https://www.coingecko.com/"
+          rel="noopener noreferrer"
+        >
+          CoinGecko
+        </a>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
